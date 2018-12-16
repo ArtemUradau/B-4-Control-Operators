97b535cfed6b889a6fb6266d563f63cc15343e11
ArtemUradau <art.urad1998@gmail.com>
2018-12-16 16:13:43 +0300

    Add project files.

---
+++ B-4-Control-Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Lesson_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lesson.LessonExampleWhile();
+
+            Practice.B4_P1_25_If_TimeOfDayGreeting();
+            Console.WriteLine("////////////////////");
+
+            Practice.B4_P2_25_If_NumbersComparing();
+            Console.WriteLine("////////////////////");
+
+            Console.ReadLine();
+        }
+     }
+}
